43907ff64d25b9b9faf18dbca9d5cb83f1b10d5c
Cristian Rojas <cristian.rojas@live.fr>
2025-05-08 01:04:09 +0200

    Move run command into examples folder

---
+++ example/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/homebrew/bin/livereloadx -s '.' --include '*.{swift,js,html}'
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/homebrew/bin/livereloadx -s './input' --include '*.{swift,js,html}'
